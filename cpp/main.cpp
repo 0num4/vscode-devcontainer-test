9a597b574158720df5454d24ecc0b55dfc847952
0num4 <r.oonuma@matsuri-tech.com>
2023-12-03 00:33:18 +0000

    feat: add devcontainer.json extension

---
+++ cpp/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "mahjongsoul.pb.h"
+
+int main() {
+//   Person person;
+//   person.set_name("Alice");
+//   person.set_id(123);
+//   person.set_email("alice@example.com");
+
+//   std::cout << "Name: " << person.name() << std::endl;
+//   std::cout << "ID: " << person.id() << std::endl;
+//   std::cout << "Email: " << person.email() << std::endl;
+    
+
+  return 0;
+}
