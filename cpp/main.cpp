d87b92de2ae3e1616b778b3c590a88047889968d
0num4 <r.oonuma@matsuri-tech.com>
2023-12-03 08:35:43 +0000

    feat: まぁまぁ強引だがとりあえずcppのデバッグ環境出来たので(本当はlaunch.jsonなど変数に置きたい)

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include "mahjongsoul.pb.h"
+// #include "mahjongsoul.pb.h"
+#include <vector>
 
 int main() {
 //   Person person;
@@ -10,7 +11,16 @@ int main() {
 //   std::cout << "Name: " << person.name() << std::endl;
 //   std::cout << "ID: " << person.id() << std::endl;
 //   std::cout << "Email: " << person.email() << std::endl;
-    printf("helloworld");
+    std::vector<int> numbers = {1, 2, 3, 4, 5};
+    int sum = 0;
+
+    for (int i = 0; i < numbers.size(); ++i) {
+        sum += numbers[i];
+        std::cout << "現在の合計: " << sum << std::endl;
+    }
+
+    std::cout << "最終合計: " << sum << std::endl;
+    return 0;
 
   return 0;
 }
