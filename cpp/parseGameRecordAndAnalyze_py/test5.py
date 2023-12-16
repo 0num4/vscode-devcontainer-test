8de027d72fe3af76cf7a3bfa00cbbeec62552473
0num4 <r.oonuma@matsuri-tech.com>
2023-12-16 08:35:30 +0000

    feat: error出るけど一応

--- cpp/parseGameRecordAndAnalyze_py/test5.py
+++ cpp/parseGameRecordAndAnalyze_py/test5.py
@@ -4,6 +4,9 @@ import base64
 from google.protobuf.descriptor import FieldDescriptor
 
 def convert_json_to_protobuf(json_data, proto_class):
+    if isinstance(proto_class, str):
+        # プロトバフのクラス名が指定された場合は、そのクラスを取得
+        proto_class = getattr(mahjongsoul_pb2, proto_class)
     proto_instance = proto_class()
 
     # JSON の各フィールドを確認
@@ -13,10 +16,15 @@ def convert_json_to_protobuf(json_data, proto_class):
         if field:
             if field.type == FieldDescriptor.TYPE_BYTES:
                 encoded_json_field_value = json.dumps(field_value).encode('utf-8')
-                print(encoded_json_field_value)
+                print(f"encoded_json_field_value: {encoded_json_field_value}")
                 setattr(proto_instance, field_name, encoded_json_field_value)
+            elif field.type == FieldDescriptor.TYPE_MESSAGE:
+                # フィールドがメッセージ型の場合は再帰的に変換
+                print(f"field.message_type.name: {field.message_type.name} field_name: {field_name}, field_value: f{field_value}")
+                setattr(proto_instance, field_name, convert_json_to_protobuf(field_value, field.message_type.name))
             else:
                 # それ以外の場合は値を直接設定
+                print(f"field_name: {field_name}, field_value: {field_value}")
                 setattr(proto_instance, field_name, field_value)
 
     return proto_instance
@@ -30,8 +38,10 @@ json_data = {
     }
 }
 
+file = open("/workspaces/vscode-devcontainer-test/cpp/parseGameRecordAndAnalyze_py/paifu/mahjongsoul_paifu_231214-df604b7c-fc99-425c-8a3c-5ad0bed89a65.json", "r")
+json_data = json.load(file)
 # JSON データを Protobuf に変換
-protobuf_data = convert_json_to_protobuf(json_data, mahjongsoul_pb2.Wrapper)
+protobuf_data = convert_json_to_protobuf(json_data, mahjongsoul_pb2.ResGameRecord)
 
 # 結果の確認
 print(protobuf_data)
